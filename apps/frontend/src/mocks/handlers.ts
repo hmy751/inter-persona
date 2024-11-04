74bb1562edf54a9c3120335c6949dcad69665dfb
hmy751 <hmy751.dev@gmail.com>
2024-11-05 01:54:23 +0900

    feat: fetchAIChat msw handler 추가

--- apps/frontend/src/mocks/handlers.ts
+++ apps/frontend/src/mocks/handlers.ts
@@ -3,7 +3,7 @@ import { http, HttpResponse, RequestHandler } from "msw";
 import { baseURL } from "@/apis";
 import { LoginBody } from "@/apis/user";
 
-import { InterviewBody } from "@/apis/interview";
+import { InterviewBody, AIChatBody } from "@/apis/interview";
 
 const userHandler = [
   http.post<never, LoginBody>(`${baseURL}/login`, async ({ request }) => {
@@ -23,6 +23,15 @@ const interviewHandler = [
       const { interviewerId, reviewerId } = await request.json();
       return HttpResponse.json({
         id: 1,
+      });
+    }
+  ),
+  http.post<never, AIChatBody>(
+    `${baseURL}/interview/:chatId/contents`,
+    async ({ request }) => {
+      const { chatId, content } = await request.json();
+      return HttpResponse.json({
+        content: "자바스크립트의 클로저에 대해 설명해주세요",
       });
     }
   ),
