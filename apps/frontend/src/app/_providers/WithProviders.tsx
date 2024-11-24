ea4d7087faea77e0bbc48d20fe170ed29bf47913
hmy751 <hmy751@gmail.com>
2024-11-25 02:24:05 +0900

    feat: ErrorToast 및 useErrorToastStore 구현

--- apps/frontend/src/app/_providers/WithProviders.tsx
+++ apps/frontend/src/app/_providers/WithProviders.tsx
@@ -1,18 +1,18 @@
 import { ReactNode } from "react";
 
-import AudioPlayer from "@/components/AudioPlayer";
-
 import ChakraProvider from "./ChakraProvider";
 import ReduxStoreProvider from "./ReduxStoreProvider";
 import QueryProviders from "./QueryProvider";
 import { theme } from "@/styles/theme";
+
+import ErrorToast from "../_components/ErrorToast";
 
 export default function WithProviders({ children }: { children: ReactNode }) {
   return (
     <QueryProviders>
       <ChakraProvider theme={theme} resetCSS={false}>
         <ReduxStoreProvider>{children}</ReduxStoreProvider>
-        <AudioPlayer />
+        <ErrorToast />
       </ChakraProvider>
     </QueryProviders>
   );
