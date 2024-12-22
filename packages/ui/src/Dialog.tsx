051118aa239d3d90e4f768f480876bc2f38f775c
함명연 <hammyeong-yeon@hammyeong-yeon-ui-MacBookPro.local>
2024-12-23 00:45:09 +0900

    refactor: 공통 Dialog 컴포넌트 커스텀으로 구현 및 스토리북 수정

--- packages/ui/src/Dialog.tsx
+++ packages/ui/src/Dialog.tsx
@@ -1,27 +1,71 @@
-import { ReactNode, ReactElement } from "react";
+import {
+  ReactNode,
+  ReactElement,
+  useMemo,
+  useEffect,
+  useState,
+  createContext,
+  useContext,
+} from "react";
+import { createPortal } from "react-dom";
 import styles from "./Dialog.module.css";
-import * as D from "@radix-ui/react-dialog";
+import Text from "./Text";
+interface DialogContextType {
+  open: boolean;
+  setOpen: (open: boolean) => void;
+}
 
 interface DialogProps {
   children: ReactNode;
 }
 
+const DialogContext = createContext<DialogContextType>({
+  open: false,
+  setOpen: () => {},
+});
+
 export default function Dialog({ children }: DialogProps): ReactElement {
-  return <D.Root>{children}</D.Root>;
+  const [open, setOpen] = useState(false);
+
+  const value = useMemo(() => ({ open, setOpen }), [open, setOpen]);
+
+  return (
+    <DialogContext.Provider value={value}>{children}</DialogContext.Provider>
+  );
 }
 
-Dialog.Content = ({ children }: { children: ReactNode }) => (
-  <D.Portal>
-    <D.Overlay className={styles.overlay} />
-    <D.Content className={styles.content}>{children}</D.Content>
-  </D.Portal>
-);
-Dialog.Trigger = ({ children }: { children: ReactNode }) => (
-  <D.Trigger>{children}</D.Trigger>
-);
-Dialog.Title = ({ children }: { children: ReactNode }) => (
-  <D.Title className={styles.title}>{children}</D.Title>
-);
-Dialog.Description = ({ children }: { children: ReactNode }) => (
-  <D.Description className={styles.description}>{children}</D.Description>
-);
+Dialog.Trigger = ({ children }: { children: ReactNode }) => {
+  const { setOpen } = useContext(DialogContext);
+  return <div onClick={() => setOpen(true)}>{children}</div>;
+};
+
+Dialog.Content = ({ children }: { children: ReactNode }) => {
+  const { open, setOpen } = useContext(DialogContext);
+
+  // ESC key 클로즈 용도로 이벤트 등록
+  useEffect(() => {
+    const handleEscape = (e: KeyboardEvent) => {
+      if (e.key === "Escape") setOpen(false);
+    };
+
+    document.addEventListener("keydown", handleEscape);
+    return () => document.removeEventListener("keydown", handleEscape);
+  }, [setOpen]);
+  if (!open) return null;
+
+  return createPortal(
+    <>
+      <div className={styles.overlay} onClick={() => setOpen(false)} />
+      <div className={styles.content}>{children}</div>
+    </>,
+    document.body
+  );
+};
+
+Dialog.Title = ({ children }: { children: ReactNode }) => {
+  return <Text as="h2">{children}</Text>;
+};
+
+Dialog.Description = ({ children }: { children: ReactNode }) => {
+  return <Text as="p">{children}</Text>;
+};
