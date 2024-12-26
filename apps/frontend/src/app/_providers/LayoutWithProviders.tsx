df9e38330a2cd558d161b0b8e1556551f3be5910
함명연 <hammyeong-yeon@hammyeong-yeon-ui-MacBookPro.local>
2024-12-26 16:54:35 +0900

    chore: LayoutWithProviders으로 파일명 변경

--- apps/frontend/src/app/_providers/LayoutWithProviders.tsx
+++ apps/frontend/src/app/_providers/LayoutWithProviders.tsx
@@ -1,11 +1,13 @@
-import { ReactNode } from "react";
-
 import ReduxStoreProvider from "./ReduxStoreProvider";
 import QueryProviders from "./QueryProvider";
 
 import ErrorToast from "../_components/ErrorToast";
 
-export default function WithProviders({ children }: { children: ReactNode }) {
+export default function LayoutWithProviders({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <QueryProviders>
       <ReduxStoreProvider>{children}</ReduxStoreProvider>
