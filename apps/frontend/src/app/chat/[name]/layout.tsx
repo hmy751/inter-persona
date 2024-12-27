d4ebe802d22e3e1f8ccb97c869a03285d57c5c54
함명연 <hammyeong-yeon@hammyeong-yeon-ui-MacBookPro.local>
2024-12-27 16:26:11 +0900

    refactor: 면접 페이지, 레이아웃 스타일 및 컴포넌트 요소들을 공통 컴포너트로 적용

--- apps/frontend/src/app/chat/[name]/layout.tsx
+++ apps/frontend/src/app/chat/[name]/layout.tsx
@@ -1,22 +1,3 @@
-"use client";
-import Avatar from "@repo/ui/Avatar";
-import useUserStore from "@/store/useUserStore";
-import styles from "./layout.module.css";
-
 export default function Layout({ children }: { children: React.ReactNode }) {
-  const { user } = useUserStore();
-
-  return (
-    <div className={styles.container}>
-      <header className={styles.header}>
-        {user && (
-          <div className={styles.userInfo}>
-            <Avatar src={user?.imageSrc} />
-            <p className={styles.userName}>{user?.name}</p>
-          </div>
-        )}
-      </header>
-      <section className={styles.content}>{children}</section>
-    </div>
-  );
+  return <div className={"page-layout-container"}>{children}</div>;
 }
