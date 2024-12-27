7b95b017a03811e26e6c3cf59a4f99eedb61af4a
함명연 <hammyeong-yeon@hammyeong-yeon-ui-MacBookPro.local>
2024-12-27 15:14:20 +0900

    refactor: 인터뷰어 선택 페이지, 인터뷰어 클릭 이벤트 관련 로직 카드로 분리

--- apps/frontend/src/app/interviewer/_components/InterviewerCard.tsx
+++ apps/frontend/src/app/interviewer/_components/InterviewerCard.tsx
@@ -1,8 +1,13 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./InterviewerCard.module.css";
 import Text from "@repo/ui/Text";
 import Button from "@repo/ui/Button";
+import { useInterviewerStore } from "@/store/useInterviewerStore";
+import { useRouter } from "next/navigation";
 interface InterviewerCardProps {
+  id: number;
   imgUrl: string;
   name: string;
   mbti: string;
@@ -10,11 +15,27 @@ interface InterviewerCardProps {
 }
 
 export default function InterviewerCard({
+  id,
   imgUrl,
   name,
   mbti,
   description,
 }: InterviewerCardProps): React.ReactElement {
+  const setInterviewer = useInterviewerStore((state) => state.setInterviewer);
+  const router = useRouter();
+  const handleClick = () => {
+    setInterviewer({
+      id,
+      name,
+      imgUrl,
+      mbti,
+      description,
+    });
+    router.push(`/chat/31`);
+  };
+
+  console.log(id, name, imgUrl, mbti, description);
+
   return (
     <div className={styles.wrapper}>
       <Image src={imgUrl} alt={name} height={150} width={150} />
@@ -29,7 +50,7 @@ export default function InterviewerCard({
           {mbti}
         </Text>
         <div className={styles.buttonWrapper}>
-          <Button variant="outline" size="md">
+          <Button variant="outline" size="md" onClick={handleClick}>
             Start interview
           </Button>
         </div>
