60cf9555c76a54358e78f8e7f063bc4d554f273c
함명연 <hammyeong-yeon@hammyeong-yeon-ui-MacBookPro.local>
2024-12-27 20:51:18 +0900

    feat: 면접 결과 페이지, 프로그래스 바 컴포넌트 구현

--- apps/frontend/src/app/result/_components/ScoreSection.tsx
+++ apps/frontend/src/app/result/_components/ScoreSection.tsx
@@ -1,5 +1,6 @@
 import Text from "@repo/ui/Text";
 import styles from "./ScoreSection.module.css";
+import ProgressBar from "./ProgressBar";
 
 interface ScoreSectionProps {}
 
@@ -7,21 +8,24 @@ export default function ScoreSection({}: ScoreSectionProps): React.ReactElement 
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
-        <Text as="h3" size="md">
-          Score
-        </Text>
-        <Text size="lg" align="right" weight="bold">
-          80%
-        </Text>
+        <div className={styles.scoreWrapper}>
+          <Text as="h3" size="md">
+            Score
+          </Text>
+          <Text size="lg" align="right" weight="bold">
+            80%
+          </Text>
+        </div>
+        <div className={styles.scoreWrapper}>
+          <Text as="h3" size="md">
+            Question
+          </Text>
+          <Text size="lg" align="right" weight="bold">
+            12
+          </Text>
+        </div>
       </div>
-      <div className={styles.wrapper}>
-        <Text as="h3" size="md">
-          Question
-        </Text>
-        <Text size="lg" align="right" weight="bold">
-          12
-        </Text>
-      </div>
+      <ProgressBar score={80} duration={1500} />
     </div>
   );
 }
