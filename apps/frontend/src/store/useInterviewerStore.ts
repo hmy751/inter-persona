7dedaabecc432d5d1616ffaa24596fcb0a8cd4b5
함명연 <hammyeong-yeon@hammyeong-yeon-ui-MacBookPro.local>
2024-12-27 15:43:30 +0900

    chore: interviewerList 목업 데이터로 연동

--- apps/frontend/src/store/useInterviewerStore.ts
+++ apps/frontend/src/store/useInterviewerStore.ts
@@ -1,5 +1,31 @@
 import { create } from "zustand";
 
+const interviewerList = [
+  {
+    id: 1,
+    imgUrl: "/images/ENFP.webp",
+    name: "민지",
+    mbti: "ENFP",
+    description:
+      "이론 중심의 면접을 진행합니다. 컴퓨터 과학(CS) 개념과 프레임워크의 운영 원리에 관한 질문을 강조합니다.",
+  },
+  {
+    id: 2,
+    imgUrl: "/images/ESTJ.webp",
+    name: "철수",
+    mbti: "ESTJ",
+    description:
+      "이론 중심의 면접을 진행합니다. 컴퓨터 과학(CS) 개념과 프레임워크의 운영 원리에 관한 질문을 강조합니다.",
+  },
+  {
+    id: 3,
+    imgUrl: "/images/ISFP.webp",
+    name: "영희",
+    mbti: "ISFP",
+    description:
+      "이론 중심의 면접을 진행합니다. 컴퓨터 과학(CS) 개념과 프레임워크의 운영 원리에 관한 질문을 강조합니다.",
+  },
+];
 interface Interviewer {
   id: number;
   name: string;
@@ -10,10 +36,10 @@ interface Interviewer {
 
 interface InterviewerState {
   interviewer: Interviewer | null;
-  setInterviewer: (interviewer: Interviewer | null) => void;
+  setInterviewerId: (id: number) => void;
 }
 
 export const useInterviewerStore = create<InterviewerState>((set) => ({
   interviewer: null,
-  setInterviewer: (interviewer: Interviewer | null) => set({ interviewer }),
+  setInterviewerId: (id: number) => set({ interviewer: interviewerList[id - 1] }),
 }));
