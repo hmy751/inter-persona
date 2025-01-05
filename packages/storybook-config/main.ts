ee3ae52eeba62318f1f90c352ad250f75e7dbb62
함명연 <hammyeong-yeon@hammyeong-yeon-ui-MacBookPro.local>
2025-01-05 23:26:45 +0900

    chore: storybook-config 레포 설정

---
+++ packages/storybook-config/main.ts
@@ -0,0 +1,24 @@
+import { getAbsolutePath } from "./util";
+
+/**
+ * This function is used to resolve the absolute path of a package.
+ * It is needed in projects that use Yarn PnP or are set up within a monorepo.
+ */
+
+/** @type { import('@storybook/nextjs').StorybookConfig } */
+const baseConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    getAbsolutePath("@storybook/addon-onboarding"),
+    getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@chromatic-com/storybook"),
+    getAbsolutePath("@storybook/addon-interactions"),
+  ],
+  framework: {
+    name: getAbsolutePath("@storybook/nextjs"),
+    options: {},
+  },
+  staticDirs: ["../public"],
+};
+
+export default baseConfig;
