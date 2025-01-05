3476be1d9269737ffbdfec5dbd0b41350ebff716
함명연 <hammyeong-yeon@hammyeong-yeon-ui-MacBookPro.local>
2025-01-05 23:32:42 +0900

    chore: storybook-config config 적용하여 repo/ui storybook config 수정

--- packages/ui/.storybook/preview.tsx
+++ packages/ui/.storybook/preview.tsx
@@ -1,17 +1,7 @@
-import React from "react";
-import type { Preview } from "@storybook/react";
-import "@repo/ui/styles/globals.css";
+import basicPreview from "@repo/storybook-config/preview";
 
-const preview: Preview = {
-  parameters: {
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
-      },
-    },
-  },
-  decorators: [(Story) => <Story />],
+const preview = {
+  ...basicPreview,
 };
 
 export default preview;
