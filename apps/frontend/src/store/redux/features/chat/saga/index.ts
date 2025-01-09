8d6af020faed95fc835f37892d49fa489bd58997
함명연 <hammyeong-yeon@hammyeong-yeon-ui-MacBookPro.local>
2025-01-09 17:50:58 +0900

    refactor: watchRetry saga 추가

--- apps/frontend/src/store/redux/features/chat/saga/index.ts
+++ apps/frontend/src/store/redux/features/chat/saga/index.ts
@@ -1,5 +1,5 @@
 import { takeLatest } from "redux-saga/effects";
-import { SEND_RECORD, START_CHAT } from "../slice";
+import { SEND_RECORD, START_CHAT, REQUEST_INTERVIEW } from "../slice";
 
 import { requestInterviewSaga } from "./requestInterviewSaga";
 import { speechToTextSaga } from "./speechToTextSaga";
@@ -10,4 +10,8 @@ export function* watchRecord() {
 
 export function* watchStartChat() {
   yield takeLatest(START_CHAT, requestInterviewSaga);
+}
+
+export function* watchRetry() {
+  yield takeLatest(REQUEST_INTERVIEW, requestInterviewSaga);
 }
