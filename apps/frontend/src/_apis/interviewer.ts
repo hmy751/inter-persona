2ffe6af4f3fa64364982d0523c2290c47b57f730
함명연 <hammyeong-yeon@hammyeong-yeon-ui-MacBookPro.local>
2025-02-15 20:31:39 +0900

    refactor: interviewer api, react-query data훅으로 수정 및 적용

--- apps/frontend/src/_apis/interviewer.ts
+++ apps/frontend/src/_apis/interviewer.ts
@@ -1,10 +1,10 @@
 import fetcher from "./fetcher";
 import { Interviewer } from "./model";
 
-export interface InterviewerResponse {
+export interface InterviewerListResponse {
   list: Interviewer[];
 }
 
-export const fetchInterviewer = async () => {
-  return fetcher.get<InterviewerResponse>("interviewer");
+export const fetchInterviewerList = async () => {
+  return fetcher.get<InterviewerListResponse>("interviewer");
 };
