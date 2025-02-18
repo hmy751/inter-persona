100ae786e13bd626ef7ca0f0636d07b2f33bf47a
함명연 <hammyeong-yeon@hammyeong-yeon-ui-MacBookPro.local>
2025-02-18 18:33:12 +0900

    refactor: 인터뷰어 정보 페이지, 페이지 에러처리 수정

--- apps/frontend/src/app/interviewer/page.tsx
+++ apps/frontend/src/app/interviewer/page.tsx
@@ -4,9 +4,21 @@ import InterviewerCard from "@/_components/pages/interviewer/InterviewerCard";
 import styles from "./page.module.css";
 import Text from "@repo/ui/Text";
 import { useGetInterviewerList } from "@/_data/interviewer";
+import { APIError } from "@/_apis/fetcher";
 
 export default function InterviewerChoicePage() {
-  const { data } = useGetInterviewerList();
+  const { data, isLoading, error } = useGetInterviewerList();
+
+  if (isLoading) return <div>Loading...</div>;
+
+  if (error) {
+    throw new APIError(
+      "인터뷰어 목록 조회에 실패했습니다. 다시 시도해주세요.",
+      404,
+      "NOT_FOUND",
+      error
+    );
+  }
 
   return (
     <div className={styles.container}>
