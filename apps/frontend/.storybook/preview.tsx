eb1c2610a58546c953b6fbc2cf35b2f5c2310e74
함명연 <hammyeong-yeon@hammyeong-yeon-ui-MacBookPro.local>
2025-02-20 17:37:24 +0900

    refactor: 앱 스토리북, redux, reqct query 프로바이더 preview에 적용

--- apps/frontend/.storybook/preview.tsx
+++ apps/frontend/.storybook/preview.tsx
@@ -1,7 +1,22 @@
 import basicPreview from "@repo/storybook-config/preview";
+import { Provider } from "react-redux";
+import { makeStore } from "@/_store/redux/rootStore";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+const store = makeStore();
 
 const preview = {
   ...basicPreview,
+  decorators: [
+    (Story: any) => (
+      <Provider store={store}>
+        <QueryClientProvider client={queryClient}>
+          <Story />
+        </QueryClientProvider>
+      </Provider>
+    ),
+  ],
 };
 
 export default preview;
