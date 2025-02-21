7e812661d140e4b945773a021490e3e568444f0c
함명연 <hammyeong-yeon@hammyeong-yeon-ui-MacBookPro.local>
2025-02-21 12:39:15 +0900

    refactor: 리액트 쿼리 프로바이더 수정 및 테스트 목킹 프로바이더에 추가

--- apps/frontend/src/_tests/_mocks/providers.tsx
+++ apps/frontend/src/_tests/_mocks/providers.tsx
@@ -1,12 +1,15 @@
 import React, { PropsWithChildren } from "react";
 import { render, RenderOptions, RenderResult } from "@testing-library/react";
 import { Provider } from "react-redux";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createQueryClient } from "@/_components/layout/providers/QueryProvider";
 import { makeStore } from "@/_store/redux/rootStore";
 import type { AppStore, RootState } from "@/_store/redux/rootStore";
 
 interface ExtendedRenderOptions extends Omit<RenderOptions, "queries"> {
   preloadedState?: Partial<RootState>;
   store?: AppStore;
+  queryClient?: QueryClient;
 }
 
 export function renderWithProviders(
@@ -14,11 +17,18 @@ export function renderWithProviders(
   {
     preloadedState = {},
     store = makeStore(preloadedState),
+    queryClient = createQueryClient(),
     ...renderOptions
   }: ExtendedRenderOptions = {}
-): RenderResult & { store: AppStore } {
+): RenderResult & { store: AppStore; queryClient: QueryClient } {
   function Wrapper({ children }: PropsWithChildren): JSX.Element {
-    return <Provider store={store}>{children}</Provider>;
+    return (
+      <Provider store={store}>
+        <QueryClientProvider client={queryClient}>
+          {children}
+        </QueryClientProvider>
+      </Provider>
+    );
   }
 
   const renderResult = render(ui, { wrapper: Wrapper, ...renderOptions });
@@ -26,5 +36,6 @@ export function renderWithProviders(
   return {
     ...renderResult,
     store,
+    queryClient,
   };
 }
