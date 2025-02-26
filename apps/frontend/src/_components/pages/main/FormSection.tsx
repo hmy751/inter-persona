0740aa6a32f57eba8c8735d79250a06220f707f4
함명연 <hammyeong-yeon@hammyeong-yeon-ui-MacBookPro.local>
2025-02-26 18:03:43 +0900

    feat: 메인 페이지, Form 요소 상태 로직 수정

--- apps/frontend/src/_components/pages/main/FormSection.tsx
+++ apps/frontend/src/_components/pages/main/FormSection.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useMutation } from "@tanstack/react-query";
 import { fetchLogin } from "@/_apis/user";
 import { useRouter } from "next/navigation";
@@ -10,14 +10,53 @@ import Button from "@repo/ui/Button";
 import styles from "./FormSection.module.css";
 import useUserStore from "@/_store/zustand/useUserStore";
 
+const validateEmail = (email: string) => {
+  if (!email) return "이메일을 입력해주세요.";
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email)) return "이메일 형식으로 입력해주세요.";
+  return "";
+};
+
+const validatePassword = (password: string) => {
+  if (!password) return "비밀번호를 입력해주세요.";
+  if (password.length < 10) return "비밀번호는 최소 10자 이상이어야 합니다.";
+
+  const hasUpperCase = /[A-Z]/.test(password);
+  const hasLowerCase = /[a-z]/.test(password) && /[a-z].*[a-z]/.test(password); // 소문자 최소 2개
+  const hasDigit = /\d/.test(password);
+
+  if (!(hasUpperCase && hasLowerCase && hasDigit)) {
+    return "비밀번호는 대문자 1개 이상, 소문자 2개 이상, 숫자 1개 이상을 포함해야 합니다.";
+  }
+
+  return "";
+};
+
 export default function FormSection() {
   const router = useRouter();
   const { setUser } = useUserStore();
-  const [formData, setFormData] = useState({
-    email: "",
-    name: "",
-    imageSrc: "",
-  });
+
+  const [email, setEmail] = useState("");
+  const [emailFocused, setEmailFocused] = useState(false);
+  const [emailTouched, setEmailTouched] = useState(false);
+  const [emailError, setEmailError] = useState("");
+
+  useEffect(() => {
+    if (emailTouched) {
+      setEmailError(validateEmail(email));
+    }
+  }, [email, emailTouched]);
+
+  const [password, setPassword] = useState("");
+  const [passwordFocused, setPasswordFocused] = useState(false);
+  const [passwordTouched, setPasswordTouched] = useState(false);
+  const [passwordError, setPasswordError] = useState("");
+
+  useEffect(() => {
+    if (passwordTouched) {
+      setPasswordError(validatePassword(password));
+    }
+  }, [password, passwordTouched]);
 
   const loginMutation = useMutation({
     mutationFn: fetchLogin,
@@ -30,48 +69,53 @@ export default function FormSection() {
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    loginMutation.mutate(formData);
+
+    if (!emailTouched) setEmailTouched(true);
+    if (!passwordTouched) setPasswordTouched(true);
+
+    const emailError = validateEmail(email);
+    const passwordError = validatePassword(password);
+
+    if (emailError) {
+      setEmailError(emailError);
+    }
+
+    if (passwordError) {
+      setPasswordError(passwordError);
+    }
+
+    if (emailError || passwordError) return;
+
+    loginMutation.mutate({ email, password });
   };
 
   return (
-    <form className={styles.form}>
-      {/* <div className={styles.inputGroup}>
-    <label className={styles.label}>이름</label>
-    <input
-      type="text"
-      name="name"
-      className={styles.input}
-      value={formData.name}
-      onChange={(e) => setFormData({ ...formData, name: e.target.value })}
-    />
-  </div> */}
-      <InputField label="이름">
+    <form className={styles.form} onSubmit={handleSubmit}>
+      <InputField label="이메일" message={emailError}>
         <Input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
           placeholder="Enter Text..."
-          // isFocused={isFocused}
-          // isTouched={isTouched}
-          // onFocusChange={setIsFocused}
-          // onTouchChange={setIsTouched}
+          isFocused={emailFocused}
+          isTouched={emailTouched}
+          isError={emailError}
+          onFocusChange={setEmailFocused}
+          onTouchChange={setEmailTouched}
         />
       </InputField>
 
-      {/* <div className={styles.inputGroup}>
-    <label className={styles.label}>이메일</label>
-    <input
-      type="email"
-      name="email"
-      className={styles.input}
-      value={formData.email}
-      onChange={(e) => setFormData({ ...formData, email: e.target.value })}
-    />
-  </div> */}
-      <InputField label="이메일">
+      <InputField label="비밀번호" message={passwordError}>
         <Input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
           placeholder="Enter Text..."
-          // isFocused={isFocused}
-          // isTouched={isTouched}
-          // onFocusChange={setIsFocused}
-          // onTouchChange={setIsTouched}
+          isFocused={passwordFocused}
+          isTouched={passwordTouched}
+          isError={passwordError}
+          onFocusChange={setPasswordFocused}
+          onTouchChange={setPasswordTouched}
         />
       </InputField>
 
