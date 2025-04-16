9ea632a4c9ef469a9f9814aea2660798313faa6d
함명연 <hammyeong-yeon@hammyeong-yeon-ui-MacBookPro.local>
2025-04-16 17:55:14 +0900

    refactor: @repo/testing-config tsup 빌드 변경 및 관련 속성 수정

--- packages/testing-config/src/jest.config.base.ts
+++ packages/testing-config/src/jest.config.base.ts
@@ -1,7 +1,8 @@
-import { getTestingConfigPath } from "./util";
+import { getTestingConfigPath, getMockPath } from "./util";
 
 const baseConfig = {
   verbose: true,
+  // preset: "ts-jest",
   collectCoverageFrom: [
     "**/*.{js,jsx,ts,tsx}",
     "!**/*.d.ts",
@@ -13,11 +14,11 @@ const baseConfig = {
     "\\.(css|less|scss|sass)\\?modules$": "identity-obj-proxy",
 
     // testing-config 경로 수정
-    "^.+\\.(css|sass|scss)$": getTestingConfigPath("mocks/styleMock.js"),
-    "^.+\\.(jpg|jpeg|png|gif|webp|svg)$": getTestingConfigPath("mocks/fileMock.js"),
+    "^.+\\.(css|sass|scss)$": getMockPath("styleMock.js"),
+    "^.+\\.(jpg|jpeg|png|gif|webp|svg)$": getMockPath("fileMock.js"),
   },
   transform: {
-    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
+    // "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       getTestingConfigPath("jest.transform.base.ts"),
   },
---
+++ packages/testing-config/src/util.ts
@@ -0,0 +1,17 @@
+import path from "path";
+
+const getRootDir = () => {
+  return path.resolve(process.cwd(), '../..')
+}
+
+const getMockPath = (pathString: string) => {
+  return path.join(getRootDir(), 'packages/testing-config/src/mocks', pathString)
+}
+
+const getTestingConfigPath = (pathString: string) => {
+  const distDir = path.resolve(__dirname, '..', 'dist');
+  const jsFilename = pathString.replace(/\.ts$/, '.js');
+  return path.resolve(distDir, jsFilename);
+}
+
+export { getRootDir, getMockPath, getTestingConfigPath }
