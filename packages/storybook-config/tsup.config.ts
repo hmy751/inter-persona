44753c22ad289196084c83f7bff4e3789fa12f8d
함명연 <hammyeong-yeon@hammyeong-yeon-ui-MacBookPro.local>
2025-04-16 15:00:30 +0900

    fix: repo/storybook-config, 타입 빌드에 따라 tsconfig.json 경로 및 속성 수정

--- packages/storybook-config/tsup.config.ts
+++ packages/storybook-config/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ["main.ts", "preview.tsx", "util.ts"],
+  entry: ["main.ts", "preview.tsx", "util.ts", "src/**/*.ts", "src/**/*.tsx"],
   format: ["cjs", "esm"],
   dts: true,
   clean: true,
