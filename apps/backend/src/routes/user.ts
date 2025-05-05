ce9f117b8daf664ea53fef719ab2bc2977aee493
hmy751 <hmy751.dev@gmail.com>
2025-05-05 23:23:29 +0900

    chore: 상수 변수명 수정

--- apps/backend/src/routes/user.ts
+++ apps/backend/src/routes/user.ts
@@ -1,7 +1,7 @@
 import { Router, Request, Response, NextFunction } from 'express';
 import { prisma } from '@/app';
 import bcrypt from 'bcrypt';
-import { USER_ERROR, SERVER_ERROR, VALIDATION_ERROR } from '@/libs/constant';
+import { USER_ROUTE, SERVER_ERROR, VALIDATION_ERROR } from '@/libs/constant';
 import { generateToken } from '@/libs/utils';
 import { LoginSchema, RegisterSchema } from '@repo/schema/user';
 
@@ -33,20 +33,20 @@ router.post('/login', async (req: Request, res: Response, next: NextFunction) =>
     });
 
     if (!foundUser) {
-      res.status(401).json({ message: USER_ERROR.unauthorized });
+      res.status(401).json({ message: USER_ROUTE.unauthorized });
       return;
     }
 
     const isPasswordValid = await bcrypt.compare(password, foundUser.password);
 
     if (!isPasswordValid) {
-      res.status(401).json({ message: USER_ERROR.invalidCredentials });
+      res.status(401).json({ message: USER_ROUTE.invalidCredentials });
       return;
     }
 
     const token = generateToken({ id: foundUser.id.toString() });
 
-    res.json({ token });
+    res.status(201).json({ token, success: true, message: USER_ROUTE.loginSuccess });
   } catch (error) {
     console.error('Login error:', error);
     res.status(500).json({ message: SERVER_ERROR.internal });
@@ -74,7 +74,7 @@ router.post('/register', async (req: Request, res: Response, next: NextFunction)
     });
 
     if (foundUser) {
-      res.status(400).json({ message: USER_ERROR.alreadyExists });
+      res.status(400).json({ message: USER_ROUTE.alreadyExists });
       return;
     }
 
@@ -84,7 +84,7 @@ router.post('/register', async (req: Request, res: Response, next: NextFunction)
       data: { email, password: hashedPassword, name, profileImageUrl: profileImage },
     });
 
-    res.status(201).json({ success: true });
+    res.status(201).json({ success: true, message: USER_ROUTE.registerSuccess });
   } catch (error) {
     console.error('Register error:', error);
     res.status(500).json({ message: SERVER_ERROR.internal });
