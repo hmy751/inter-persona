86a3bb11808fb467b24d492bc26d706baf2f8b2c
hmy751 <hmy751.dev@gmail.com>
2025-05-05 21:17:33 +0900

    refactor: VALIDATION 메시지 상수 변수명 수정

--- packages/schema/src/user.ts
+++ packages/schema/src/user.ts
@@ -1,31 +1,31 @@
 import { z } from 'zod';
-import { ERROR_MESSAGE } from '@repo/constant/message';
+import { VALIDATION } from '@repo/constant/message';
 
 export const LoginSchema = z.object({
   email: z
     .string()
     .min(1, {
-      message: ERROR_MESSAGE.email.required,
+      message: VALIDATION.email.required,
     })
     .email({
-      message: ERROR_MESSAGE.email.invalid,
+      message: VALIDATION.email.invalid,
     }),
   password: z
     .string()
     .min(8, {
-      message: ERROR_MESSAGE.password.length,
+      message: VALIDATION.password.length,
     })
     .regex(/[a-z]/, {
-      message: ERROR_MESSAGE.password.lowercase,
+      message: VALIDATION.password.lowercase,
     })
     .regex(/[A-Z]/, {
-      message: ERROR_MESSAGE.password.uppercase,
+      message: VALIDATION.password.uppercase,
     })
     .regex(/\d/, {
-      message: ERROR_MESSAGE.password.number,
+      message: VALIDATION.password.number,
     })
     .regex(/[!@#$%^&*]/, {
-      message: ERROR_MESSAGE.password.special,
+      message: VALIDATION.password.special,
     }),
 });
 
@@ -33,35 +33,35 @@ export const RegisterSchema = z.object({
   email: z
     .string()
     .min(1, {
-      message: ERROR_MESSAGE.email.required,
+      message: VALIDATION.email.required,
     })
     .email({
-      message: ERROR_MESSAGE.email.invalid,
+      message: VALIDATION.email.invalid,
     }),
   password: z
     .string()
     .min(8, {
-      message: ERROR_MESSAGE.password.length,
+      message: VALIDATION.password.length,
     })
     .regex(/[a-z]/, {
-      message: ERROR_MESSAGE.password.lowercase,
+      message: VALIDATION.password.lowercase,
     })
     .regex(/[A-Z]/, {
-      message: ERROR_MESSAGE.password.uppercase,
+      message: VALIDATION.password.uppercase,
     })
     .regex(/\d/, {
-      message: ERROR_MESSAGE.password.number,
+      message: VALIDATION.password.number,
     })
     .regex(/[!@#$%^&*]/, {
-      message: ERROR_MESSAGE.password.special,
+      message: VALIDATION.password.special,
     }),
   name: z
     .string()
     .min(2, {
-      message: ERROR_MESSAGE.name.length,
+      message: VALIDATION.name.length,
     })
     .max(10, {
-      message: ERROR_MESSAGE.name.length,
+      message: VALIDATION.name.length,
     }),
   profileImage: z.string().optional(),
 });
