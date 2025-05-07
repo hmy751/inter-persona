b1d1157f06f814dcad396e13afd1d35e84318571
hmy751 <hmy751.dev@gmail.com>
2025-05-07 22:23:34 +0900

    feat: 인증 여부에 따른 페이지 라우팅 미들웨어 적용

--- apps/frontend/src/middleware.ts
+++ apps/frontend/src/middleware.ts
@@ -1,12 +1,32 @@
-import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-export function middleware(request: NextRequest) {
-  const token = cookies().get('token');
+// 제외할 파일 확장자들
+const FILE_EXTENSIONS = /\.(js|css|map|png|jpg|jpeg|gif|svg|ico|woff|woff2|ttf|eot)$/;
 
+export function middleware(request: NextRequest) {
   if (request.nextUrl.pathname === '/health') {
     return new NextResponse('OK', { status: 200 });
+  }
+
+  const token = request.cookies.get('token');
+
+  const path = request.nextUrl.pathname;
+  const { origin } = request.nextUrl;
+
+  // _next 경로이거나 특정 파일 확장자를 포함하는 경로는 무시
+  if (path.startsWith('/_next/') || FILE_EXTENSIONS.test(path)) {
+    return NextResponse.next();
+  }
+
+  if (token) {
+    if (path === '/main') {
+      return NextResponse.redirect(new URL('/interviewer', origin));
+    }
+  } else {
+    if (path !== '/main') {
+      return NextResponse.redirect(new URL('/main', origin));
+    }
   }
 
   return NextResponse.next();
