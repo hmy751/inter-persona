ad059dba552f342dea09cc7aa728f4c5c62b55c5
hmy751 <hmy751.dev@gmail.com>
2025-05-12 20:10:30 +0900

    refactor: repo/schema, 인터뷰 데이터 조회 스키마 및 관련 부분 수정

--- packages/schema/src/interviewer.ts
+++ packages/schema/src/interviewer.ts
@@ -1,5 +1,23 @@
 import { z } from 'zod';
 
+/**
+ * 모델 스키마
+ */
+export const InterviewerSchema = z.object({
+  id: z.number(),
+  name: z.string(),
+  description: z.string(),
+  persona: z.object({
+    mbti: z.string(),
+    style: z.string(),
+    focus: z.string(),
+  }),
+  profileImageUrl: z.string(),
+});
+
+/**
+ * 요청, 응답 스키마
+ */
 export const InterviewerRequestSchema = z.object({
   id: z.number(),
 });
