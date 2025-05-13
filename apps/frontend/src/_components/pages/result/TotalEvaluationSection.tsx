a9347fc3db07fbbcdfe15970a99af140f1793000
hmy751 <hmy751.dev@gmail.com>
2025-05-13 15:38:05 +0900

    refactor: frontend, 결과 조회 data 클라이언트에 적용

--- apps/frontend/src/_components/pages/result/TotalEvaluationSection.tsx
+++ apps/frontend/src/_components/pages/result/TotalEvaluationSection.tsx
@@ -1,11 +1,12 @@
 import styles from './TotalEvaluationSection.module.css';
 import Text from '@repo/ui/Text';
-import { useGetResultTotalEvaluation } from '@/_data/result';
-
+import { useGetResult } from '@/_data/result';
+import { useParams } from 'next/navigation';
 interface TotalEvaluationSectionProps {}
 
 export default function TotalEvaluationSection({}: TotalEvaluationSectionProps): React.ReactElement {
-  const { data, isLoading, error } = useGetResultTotalEvaluation();
+  const resultId = useParams().resultId;
+  const { data, isLoading, error } = useGetResult(Number(resultId));
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -20,17 +21,17 @@ export default function TotalEvaluationSection({}: TotalEvaluationSectionProps):
       <Text as="h3" size="md">
         Total Evaluation
       </Text>
-      {data.evaluation.map(item => (
-        <div className={styles.evaluation}>
+      {data.scores.map(item => (
+        <div className={styles.evaluation} key={item.standard}>
           <div className={styles.evaluationHeader}>
             <Text size="sm" weight="bold">
-              {item.title}
+              {item.standard}
             </Text>
             <Text size="sm" className={styles.evaluationScore}>
               {item.score}%
             </Text>
           </div>
-          <Text size="sm">{item.content}</Text>
+          <Text size="sm">{item.summary}</Text>
         </div>
       ))}
     </div>
