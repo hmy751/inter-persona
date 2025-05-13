834c2aa71ace8af94193108b3d74ce6a4cbd071f
함명연 <hmy751@gmail.com>
2025-05-13 11:43:13 +0900

    feat: repo/schema, result 스키마 작성 및 추가

--- packages/schema/tsup.config.ts
+++ packages/schema/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/index.ts', 'src/user.ts', 'src/interviewer.ts', 'src/interview.ts'],
+  entry: ['src/index.ts', 'src/user.ts', 'src/interviewer.ts', 'src/interview.ts', 'src/result.ts'],
   outDir: 'dist',
   format: ['esm', 'cjs'],
   dts: true,
