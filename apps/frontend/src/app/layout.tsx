59d8653cb482ae609bf04d2a3fdbeb05d84fbaf1
함명연 <hmy751@gmail.com>
2025-05-14 23:24:29 +0900

    feat: clarity 스크립트 적용

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Noto_Sans_KR } from 'next/font/google';
+import Script from 'next/script';
 import '@repo/ui/styles/globals.css';
 import MSWProvider from '@/_components/layout/providers/MSWProvider';
 import RootProviders from '@/_components/layout/providers/RootProviders';
@@ -15,9 +16,23 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   const isUseMsw = process.env.NEXT_PUBLIC_USE_MSW;
+  const clarityId = process.env.NEXT_PUBLIC_CLARITY;
 
   return (
     <html lang="en">
+      <head>
+        {clarityId && (
+          <Script id="clarity-script" strategy="afterInteractive">
+            {`
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "${clarityId}");
+            `}
+          </Script>
+        )}
+      </head>
       <body className={noto_sans_kr.className}>
         {!isUseMsw ? (
           <RootProviders>
