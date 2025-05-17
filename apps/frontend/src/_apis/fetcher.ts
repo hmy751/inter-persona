26cb6652d61e56f49be5ea461df650636faf14e2
함명연 <hmy751@gmail.com>
2025-05-18 02:32:32 +0900

    chore: url 출력 테스트

--- apps/frontend/src/_apis/fetcher.ts
+++ apps/frontend/src/_apis/fetcher.ts
@@ -62,6 +62,7 @@ const createHttpClient = (defaultConfig: FetcherConfig) => {
     try {
       const controller = createAbortController(options.timeout || defaultConfig.timeout);
       const fullURL = `${defaultConfig.baseURL}${url}`;
+      console.log(defaultConfig, defaultConfig?.baseURL, url, fullURL);
 
       const finalOptions: RequestConfig = {
         ...options,
