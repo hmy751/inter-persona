b25fd87ebb7551374d6955370aad295dd1580d1f
함명연 <hmy751@gmail.com>
2025-05-17 17:35:15 +0900

    refactor: @backend, tsup target node 버전 수정

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -1,15 +1,15 @@
 import config from '@/config';
 import express, { Application } from 'express';
-// import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 
-// import { authenticate } from '@/middleware/auth';
-// import userRouter from '@/routes/user';
-// import interviewerRouter from '@/routes/interviewer';
-// import interviewRouter from '@/routes/interview';
-// import resultRouter from '@/routes/result';
-// const prisma = new PrismaClient();
+import { authenticate } from '@/middleware/auth';
+import userRouter from '@/routes/user';
+import interviewerRouter from '@/routes/interviewer';
+import interviewRouter from '@/routes/interview';
+import resultRouter from '@/routes/result';
+const prisma = new PrismaClient();
 
 const app: Application = express();
 
@@ -27,23 +27,22 @@ app.get('/health', (req, res) => {
   res.status(200).send('Hello World!');
 });
 
-// app.use('/user', userRouter);
-// app.use('/interviewer', authenticate, interviewerRouter);
-// app.use('/interview', authenticate, interviewRouter);
-// app.use('/result', authenticate, resultRouter);
+app.use('/user', userRouter);
+app.use('/interviewer', authenticate, interviewerRouter);
+app.use('/interview', authenticate, interviewRouter);
+app.use('/result', authenticate, resultRouter);
 
 const server = app.listen(config.server.port, () => {
   console.log(`http://${config.server.host}:${config.server.port} 실행`);
   console.log(`env: ${config.env}`);
 });
 
-// process.on('SIGINT', async () => {
-//   await prisma.$disconnect();
-//   server.close(() => {
-//     console.log('서버 종료');
-//     process.exit(0);
-//   });
-// });
+process.on('SIGINT', async () => {
+  await prisma.$disconnect();
+  server.close(() => {
+    console.log('서버 종료');
+    process.exit(0);
+  });
+});
 
-// export { app, prisma };
-export { app };
+export { app, prisma };
