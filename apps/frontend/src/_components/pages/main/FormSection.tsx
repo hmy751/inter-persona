fcf68fddf8905a8a331ab2559a315642abe7fdcd
함명연 <hmy751@gmail.com>
2025-05-19 02:27:31 +0900

    refactor: @frontend, Form key 속성 추가

--- apps/frontend/src/_components/pages/main/FormSection.tsx
+++ apps/frontend/src/_components/pages/main/FormSection.tsx
@@ -12,17 +12,17 @@ export default function FormSection() {
 
   return (
     <>
-      {isLogin ? <LoginForm /> : <SignupForm onSuccess={() => setIsLogin(true)} />}
+      {isLogin ? <LoginForm key="loginForm" /> : <SignupForm key="signupForm" onSuccess={() => setIsLogin(true)} />}
       <div className={styles.toggleButton} onClick={() => setIsLogin(!isLogin)}>
         {isLogin ? (
-          <Text as="p" size="sm" align="center">
+          <Text key="loginButton" as="p" size="sm" align="center">
             계정이 없으신가요?
             <Button size="sm" variant="text" onClick={() => setIsLogin(false)}>
               회원가입
             </Button>
           </Text>
         ) : (
-          <Text as="p" size="sm" align="center">
+          <Text key="signupButton" as="p" size="sm" align="center">
             이미 계정이 있으신가요?{' '}
             <Button size="sm" variant="text" onClick={() => setIsLogin(true)}>
               로그인
