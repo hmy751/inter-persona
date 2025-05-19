9c9216caeb8cbca40b5535746ea8ca03d9eb3f3d
함명연 <hmy751@gmail.com>
2025-05-19 22:21:06 +0900

    chore: @frontend, 변수명 변경

--- apps/frontend/src/_components/layout/Initialize.tsx
+++ apps/frontend/src/_components/layout/Initialize.tsx
@@ -4,7 +4,7 @@ import { useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import { useFunnelIdStore } from '@/_store/zustand/useFunnelId';
 
-export default function GtmInitialize() {
+export default function Initialize() {
   const { setFunnelId, clearFunnelId } = useFunnelIdStore();
   const id = uuidv4();
 
