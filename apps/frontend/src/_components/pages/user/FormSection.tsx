0304f1be08b0d988091dae93bb8a24907e29644b
hmy751 <hmy751.dev@gmail.com>
2025-05-27 17:51:17 +0900

    chore: @frontend, 로그인 폼 전환 변수명 수정

--- apps/frontend/src/_components/pages/user/FormSection.tsx
+++ apps/frontend/src/_components/pages/user/FormSection.tsx
@@ -8,23 +8,23 @@ import Text from '@repo/ui/Text';
 import Button from '@repo/ui/Button';
 
 export default function FormSection() {
-  const [isLogin, setIsLogin] = useState(true);
+  const [isLoginForm, setIsLoginForm] = useState(true);
 
   return (
     <>
-      {isLogin ? <LoginForm key="loginForm" /> : <SignupForm key="signupForm" onSuccess={() => setIsLogin(true)} />}
-      <div className={styles.toggleButton} onClick={() => setIsLogin(!isLogin)}>
-        {isLogin ? (
+      {isLoginForm ? <LoginForm /> : <SignupForm onSuccess={() => setIsLoginForm(true)} />}
+      <div className={styles.toggleButton} onClick={() => setIsLoginForm(!isLoginForm)}>
+        {isLoginForm ? (
           <Text key="loginButton" as="p" size="sm" align="center">
             계정이 없으신가요?
-            <Button size="sm" variant="text" onClick={() => setIsLogin(false)}>
+            <Button size="sm" variant="text" onClick={() => setIsLoginForm(false)}>
               회원가입
             </Button>
           </Text>
         ) : (
           <Text key="signupButton" as="p" size="sm" align="center">
             이미 계정이 있으신가요?{' '}
-            <Button size="sm" variant="text" onClick={() => setIsLogin(true)}>
+            <Button size="sm" variant="text" onClick={() => setIsLoginForm(true)}>
               로그인
             </Button>
           </Text>
